dd644888fb4ac476b0b5964c53953a7602d20ce4
Brian Pietravalle <bpietravalle@gmail.com>
2015-08-18 12:39:19 -0400

    added initial tests for fbutil.ref; all passing

--- src/app/components/fb.auth/fbAuth.factory.spec.js
+++ src/app/components/fb.auth/fbAuth.factory.spec.js
@@ -1,17 +1,15 @@
 (function(angular) {
     "use strict";
     describe('Firebase Factory', function() {
-			var fbutil;
         beforeEach(function() {
 					MockFirebase.override();
             module('mock.firebase');
             module('fb.auth');
         });
         it("should return a $firebaseAuth instance", function() {
-            inject(function(fbAuth, $firebaseAuth){ //, _fbutil_) {
+            inject(function(fbAuth, $firebaseAuth){ 
                 var ref = new MockFirebase();
                 var testInst = $firebaseAuth(ref);
-								// var fbutil = _fbutil_;
                 expect(fbAuth.prototype === testInst.prototype).toBe(true);
             });
         });
--- src/app/components/fb.utils/fbutil.factory.spec.js
+++ src/app/components/fb.utils/fbutil.factory.spec.js
@@ -1,34 +1,51 @@
 (function(angular) {
-    "use strict";
+        "use strict";
 
-    describe('fbutil', function() {
-        var fbutil;
-        beforeEach(function() {
-            module('mock.firebase');
-            module('fb.utils');
-        });
-        beforeEach(inject(function(_fbutil_) {
-            fbutil = _fbutil_;
-        }));
+        describe('fbutil', function() {
+            var fbutil;
+            beforeEach(function() {
+                // module('mock.firebase');
+                module('fb.utils');
+            });
+            beforeEach(inject(function(_fbutil_) {
+                fbutil = _fbutil_;
+            }));
 
-        it("should exist", function() {
-            expect(fbutil).toBeDefined();
-        });
+            it("should exist", function() {
+                expect(fbutil).toBeDefined();
+            });
 
-        describe('handler', function() {
-            it('should have tests');
-        });
+            describe('handler', function() {
+                it(" fbutil.handler is a function", function() {
+                    expect(typeof fbutil.handler).toBe('function');
+                });
+            });
 
-        describe('defer', function() {
-            it('should have tests');
-        });
+            describe('defer', function() {
+                it(" fbutil.defer is a function", function() {
+                    expect(typeof fbutil.defer).toBe('function');
+                });
+            });
 
-        describe('ref', function() {
-            it("firebaseRef is a function", function() {
-                expect(typeof firebaseRef).toBe('function');
+            describe('ref', function() {
+                var FBURL;
+                beforeEach(inject(function(_FBURL_) {
+                   FBURL = _FBURL_;
+                }));
+
+                it("fbutil.ref is a function", function() {
+                    expect(typeof fbutil.ref).toBe('function');
+                });
+
+                it("ref.path returns correct URL with child path", function() {
+                    var path = "a_string";
+                    var test = fbutil.ref(path);
+                    expect(test.path).toEqual('https://your-firebase.firebaseio.com/a_string');
+                });
+                it("ref.path returns parent URL if no child path given", function() {
+                    var test = fbutil.ref();
+                    expect(test.path).toEqual(FBURL); 
+                });
             });
         });
-
-    });
-
-}(angular));
+})(angular);
