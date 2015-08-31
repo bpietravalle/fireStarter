80e08442b61796787152aff1f24752082ad95496
Brian Pietravalle <bpietravalle@gmail.com>
2015-08-31 18:45:20 -0400

    added tests to reg service

--- src/app/components/fb.services/srvc.auth/reg/reg.service.js
+++ src/app/components/fb.services/srvc.auth/reg/reg.service.js
@@ -1,62 +1,62 @@
 (function(angular) {
-        "use strict";
+    "use strict";
 
-        function RegService($q, afEntity, auth) {
+    function RegService($q, afEntity, auth) {
 
-            var authObj = afEntity.set();
+        var authObj = afEntity.set();
 
-            this.passwordAndEmailRegister = function(email, pass) {
-                return authObj
-                    .$createUser({
-                        email: email,
-                        password: pass
-                    })
-                    .then(function(userData) {
-                        return auth.passwordAndEmailLogin(email, pass);
-                    })
-                    .then(function(authData) {
-                        getUser(authData);
-                    })
-                    .then(saveAuthData(authData),
+        this.passwordAndEmailRegister = function(email, pass) {
+            return authObj
+                .$createUser({
+                    email: email,
+                    password: pass
+                })
+                .then(function(userData) {
+									
+                    return auth.passwordAndEmailLogin(email, pass);
+                })
+                .then(function(authData) {
+                    getUser(authData);
+                })
+                .then(saveAuthData(authData),
 
-                        function(error) {
-                            $q.reject(error);
-                        });
+                    function(error) {
+
+                    });
+        };
+
+
+        this.registerOAuth = function(provider) {
+            return authObj
+                .$authWithOAuthPopup(provider)
+                // need to add scope
+                .then(function(authData) {
+                        session.setAuthData(authData);
+                        return authData;
+                    },
+                    function(error) {
+                        $q.reject(error);
+                    }
+                );
+        };
+
+        function getUser(authData) {
+            if (authData) {
+                var user = afEntity.set('users', authData.uid);
+                return user.$loaded()
+            } else {
+                console.log("no authentication data available");
             }
+        }
 
 
-            this.registerOAuth = function(provider) {
-                return authObj
-                    .$authWithOAuthPopup(provider)
-                    // need to add scope
-                    .then(function(authData) {
-                            session.setAuthData(authData);
-                            return authData;
-                        },
-                        function(error) {
-                            $q.reject(error);
-                        }
-                    );
+
+        function saveAuthData(authData) {
+            if (authData.provider === "password") {
+                //save data
+            } else {
+                saveOAuthData(authData);
             };
-
-            function getUser(authData) {
-                if (authData) {
-                    var user = afEntity.set('users', authData.uid);
-                    return user.$loaded()
-                } else {
-                    console.log("no authentication data available");
-                }
-            }
-
-
-
-            function saveAuthData(authData) {
-                if (authData.provider === "password") {
-                    //save data
-                } else {
-                    saveOAuthData(authData);
-                };
-            }
         }
 
         //send this to a service to set data...
@@ -94,7 +94,7 @@
     RegService.$inject = ['$q', 'afEntity', 'auth'];
 
     angular.module('srvc.auth')
-    .service('reg', RegService);
+        .service('reg', RegService);
 
 
 })(angular);
--- src/app/components/fb.services/srvc.auth/reg/reg.service.spec.js
+++ src/app/components/fb.services/srvc.auth/reg/reg.service.spec.js
@@ -67,76 +67,81 @@
             });
             //TODO: test session.setAuthData is called
         });
-        // describe("registerOAuth", function() {
-        //     beforeeach(inject(function() {
-        //         spyOn(reg, 'registerOAuth').and.callFake(function() {
-        //             deferred = $q.defer();
-        //             return deferred.promise;
-        //         });
+        describe("registerOAuth", function() {
+            beforeEach(inject(function() {
+                spyOn(reg, 'registerOAuth').and.callFake(function() {
+                    deferred = $q.defer();
+                    return deferred.promise;
+                });
 
-        //         this.credentials = {
-        //             provider: 'provider'
-        //         };
-        //     }));
-        //     it('passes userData if promise resolves', function() {
-        //         var handler = jasmine.createSpy('handler');
-        //         var test = reg.registerOAuth(this.provider);
-        //         test.then(handler);
-        //         deferred.resolve(data);
-        //         $rootScope.$digest();
-        //         expect(handler).toHaveBeenCalledWith(data);
-        //     });
-        //     it('doesnt pass userData if promise is rejected', function() {
-        //         var handler = jasmine.createSpy('handler');
-        //         var test = reg.registerOAuth(this.provider);
-        //         test.then(handler);
-        //         deferred.reject("error");
-        //         $rootScope.$digest();
-        //         expect(handler).not.toHaveBeenCalledWith(data);
-        //     });
-        //     it('passes error message if promise is rejected', function() {
-        //         var error = jasmine.createSpy('handler');
-        //         var test = reg.registerOAuth(this.provider);
-        //         test.then(null, error);
-        //         deferred.reject("error");
-        //         $rootScope.$digest();
-        //         expect(error).toHaveBeenCalledWith("error");
-        //     });
-        // });
-        // describe("OAuth Provider functions", function() {
-        //     beforeEach(inject(function() {
-        //         spyOn(reg, "registerOAuth");
-        //     }));
-        //     describe("#googleRegister", function() {
-        //         it("calls #registerOAuth with 'google'", function() {
-        //             reg.googleRegister();
-        //             expect(reg.registerOAuth).toHaveBeenCalledWith("google");
-        //         });
-        //     });
-        // });
-        // describe("OAuth Provider functions", function() {
-        //     beforeEach(inject(function() {
-        //         spyOn(reg, "registerOAuth");
-        //     }));
-        //     describe("#facebookRegister", function() {
-        //         it("calls #registerOAuth with 'facebook'", function() {
-        //             reg.facebookRegister();
-        //             expect(reg.registerOAuth).toHaveBeenCalledWith("facebook");
-        //         });
-        //     });
-        // });
-        // describe("OAuth Provider functions", function() {
-        //     beforeEach(inject(function() {
-        //         spyOn(reg, "registerOAuth");
-        //     }));
-        //     describe("#twitterRegister", function() {
-        //         it("calls #registerOAuth with 'twitter'", function() {
-        //             reg.twitterRegister();
-        //             expect(reg.registerOAuth).toHaveBeenCalledWith("twitter");
-        //         });
-        //     });
-        // });
-        // describe("#logout", function() {
+                this.credentials = {
+                    provider: 'provider'
+                };
+            }));
+            it('passes userData if promise resolves', function() {
+                var handler = jasmine.createSpy('handler');
+                var test = reg.registerOAuth(this.provider);
+                test.then(handler);
+                deferred.resolve(data);
+                $rootScope.$digest();
+                expect(handler).toHaveBeenCalledWith(data);
+            });
+            it('doesnt pass userData if promise is rejected', function() {
+                var handler = jasmine.createSpy('handler');
+                var test = reg.registerOAuth(this.provider);
+                test.then(handler);
+                deferred.reject("error");
+                $rootScope.$digest();
+                expect(handler).not.toHaveBeenCalledWith(data);
+            });
+            it('passes error message if promise is rejected', function() {
+                var error = jasmine.createSpy('handler');
+                var test = reg.registerOAuth(this.provider);
+                test.then(null, error);
+                deferred.reject("error");
+                $rootScope.$digest();
+                expect(error).toHaveBeenCalledWith("error");
+            });
+        });
+        describe("OAuth Provider functions", function() {
+            beforeEach(inject(function() {
+                spyOn(reg, "registerOAuth");
+            }));
+            describe("#googleRegister", function() {
+                it("calls #registerOAuth with 'google'", function() {
+                    reg.googleRegister();
+                    expect(reg.registerOAuth).toHaveBeenCalledWith("google");
+                });
+            });
+        });
+        describe("OAuth Provider functions", function() {
+            beforeEach(inject(function() {
+                spyOn(reg, "registerOAuth");
+            }));
+            describe("#facebookRegister", function() {
+                it("calls #registerOAuth with 'facebook'", function() {
+                    reg.facebookRegister();
+                    expect(reg.registerOAuth).toHaveBeenCalledWith("facebook");
+                });
+            });
+        });
+        describe("OAuth Provider functions", function() {
+            beforeEach(inject(function() {
+                spyOn(reg, "registerOAuth");
+            }));
+            describe("#twitterRegister", function() {
+                it("calls #registerOAuth with 'twitter'", function() {
+                    reg.twitterRegister();
+                    expect(reg.registerOAuth).toHaveBeenCalledWith("twitter");
+                });
+            });
+        });
+				describe("private functions", function() {
+					it("works", function(){
+						expect(reg).toEqual("boom");
+					});
+				});
+        // describe("#cancelAccount", function() {
         //     beforeEach(inject(function() {
         //         regObj = jasmine.createSpy('regObj');
         //         spyOn(session, "destroy");
