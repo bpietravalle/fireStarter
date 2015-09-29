ada01dc378b3aae843391e7773545b694388cdaa
Brian Pietravalle <bpietravalle@gmail.com>
2015-09-29 07:55:08 -0400

    fixed a few more missing 'returns'

--- src/app/fb.services/srvc.dataMngr/arrMngr.service.js
+++ src/app/fb.services/srvc.dataMngr/arrMngr.service.js
@@ -55,7 +55,7 @@
                 .catch(standardError);
 
             function destroySuccess(res) {
-                res.$destroy();
+                return res.$destroy();
             }
 
         }
@@ -66,7 +66,7 @@
                 .catch(standardError);
 
             function getRecordSuccess(res) {
-                res.$getRecord(key);
+                return res.$getRecord(key);
             }
         }
 
